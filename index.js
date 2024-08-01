8780b2f1c0749ba8dc6526689d08e39837ddc50b
ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>
2024-08-01 08:57:31 +0200

    Create index.js
    Signed-off-by: ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript pour le menu hamburger
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    var menuToggle = document.getElementById('menuToggle');
+    sidebar.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
