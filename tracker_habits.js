54b691bd577aa0cac920ed52c1a54e1399fc7d7e
ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>
2024-08-01 09:25:17 +0200

    Update tracker_habits.js
    Signed-off-by: ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>

--- tracker_habits.js
+++ tracker_habits.js
@@ -317,3 +317,11 @@ var quotes = [
 
     // Affiche une citation aléatoire à chaque chargement de la page
     displayRandomQuote();
+
+// JavaScript pour le menu hamburger
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    var menuToggle = document.getElementById('menuToggle');
+    sidebar.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
