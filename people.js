b22f5593666c311a774c55c2565938d6dbf04d89
ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>
2024-08-12 08:57:30 +0200

    Update people.js
    Signed-off-by: ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>

--- people.js
+++ people.js
@@ -146,3 +146,11 @@ function updateCharacterImage(wardrobe) {
     document.getElementById('character').src = imageSrc;
     console.log("Image du personnage mise à jour :", imageSrc);
 }
+
+// JavaScript pour le menu hamburger
+document.getElementById('menuToggle').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    var menuToggle = document.getElementById('menuToggle');
+    sidebar.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+});
