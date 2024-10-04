f45c4d9aa57ff78ab92c7280939cef4c007f11f8
ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>
2024-10-04 10:48:30 +0200

    Delete main.js
    Signed-off-by: ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>

--- main.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-        console.log('Service Worker enregistré avec succès.');
-    })
-    .catch(function(error) {
-        console.error('Erreur lors de l\'enregistrement du Service Worker :', error);
-    });
-}
