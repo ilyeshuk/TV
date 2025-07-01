69a6ba79ffa6ca02661288855667efd16009dbca
ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>
2025-07-01 10:53:34 +0200

    Update index.js
    Signed-off-by: ilyeshuk <159427197+ilyeshuk@users.noreply.github.com>

--- index.js
+++ index.js
@@ -21,3 +21,15 @@ document.addEventListener('click', function(event) {
   }
 });
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(function (registration) {
+        console.log("✅ Service Worker enregistré avec succès :", registration.scope);
+      })
+      .catch(function (error) {
+        console.log("❌ Échec de l'enregistrement du Service Worker :", error);
+      });
+  });
+}
